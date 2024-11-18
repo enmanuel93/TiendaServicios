e6bec66138adbdd718cd009bae63876970470e4a
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2024-11-17 22:27:58 -0400

    prueba CI

--- StoreService/TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ StoreService/TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -18,7 +18,7 @@ namespace TiendaServicios.Api.Libro.Tests
     {
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
-            // este metodo es para llenar con data de genfu
+            // este metodo es para llenar con data de genfu --
 
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
