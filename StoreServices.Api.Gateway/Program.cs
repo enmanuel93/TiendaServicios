5685b53e0d598404000ca16a460ffc7b08d8f08e
Francisco Enmanuel Cruz Mejia <emmanuelmejia30@hotmail.com>
2025-02-26 23:20:14 +0100

    add ApiGetway

---
+++ StoreServices.Api.Gateway/Program.cs
@@ -0,0 +1,22 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.MapControllers();
+
+app.Run();
